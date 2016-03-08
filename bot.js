88058dbf3f835320e18293761e60f6b3695ca4ec
dcs617 <dsparano1@gmail.com>
2016-03-08 16:51:19 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ function respond() {
       botRegexAd=/^\/advance/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexH = /^\/mfl/; botRegexSh = /^\/shrug/; 
       botRegexWk = /^\/coaches/; botRegexCC = /^\/cc/; botRegexTr=/^\/trades/; botRegexUser=/^\/users/;
-      botRegexPro=/^\/propose/; botRegexSos=/^\/sos/; botRegexStand=/^\/standings/;
+      botRegexPro=/^\/propose/; botRegexSos=/^\/sos/; botRegexStand=/^\/standings/; botRegexTc=/^\/tc/;
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
                 "MIA","BUF","SF","WAS","NYJ","TB"]
@@ -109,6 +109,11 @@ function respond() {
     else if(request.text && botRegexStand.test(request.text)) {
     this.res.writeHead(200);
     postMessage("http://daddyleagues.com/mflpa/standings/conference");
+    this.res.end();
+  }
+    else if(request.text && botRegexTc.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://docs.google.com/forms/d/1wnm2mvLzrZdRR_oDVFrIbsgjEKVqHDeVCGBbS_jZCOs/viewform?usp=send_form");
     this.res.end();
   }
   else {
